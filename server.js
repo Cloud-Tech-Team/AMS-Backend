6d8ce07a251b91f756347cd180c3d67609a83961
Abhiram V <abhi.raa.man.v@gmail.com>
2021-08-27 22:40:33 +0530

    Add server.js

---
+++ server.js
@@ -0,0 +1,17 @@
+// mongodb
+require('./config/db')
+
+const app = require('express')()
+const port = process.env.PORT || 3000
+
+const UserRouter = require('./api/User')
+
+// For accepting post form data
+const bodyParser = require('express')
+app.use(bodyParser.json())
+
+app.use('/user', UserRouter)
+
+app.listen(port, () => {
+    console.log(`Server running on port ${port}`)
+})
