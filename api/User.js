3e5c854e5748c8eaadba32845c819c8653f1999d
Hanna Sabu Mampallil <hanblessab@gmail.com>
2021-09-08 19:35:21 +0530

    mainform

--- api/User.js
+++ api/User.js
@@ -1,8 +1,22 @@
 const express = require('express')
 const router = express.Router()
+const path=require('path')
 
 const multer =require('multer'); // form-data multipart
-const upload = multer();
+const storage=multer.diskStorage({
+    destination:function(req,res,cb){
+        cb(null,"uploads");
+    },
+    filename:function(req,file,cb){
+        cb(
+            null,
+            file.fieldname + "-" +Date.now() + path.extname(file.originalname)
+        )
+    }
+})
+const upload = multer({storage:storage});
+
+
 // mongodb user model
 const User = require('./../models/User')
 
@@ -10,12 +24,12 @@ const bcrypt = require('bcrypt')
 
 
 router.post('/signup',upload.none(), (req, res) => {
-    let {first_name, email, password } = req.body
+    let {firstName, email, password } = req.body
     console.log(req.body)
-    first_name = first_name.toString().trim();
-    email = email.toString().trim();
+    // first_name = first_name;
+    // email = email;
 
-    if (first_name == "" || email == "" || password == "" ) {
+    if (firstName == "" || email == "" || password == "" ) {
         res.json({
             status: "FAILED",
             message: "Empty input field(s)"
@@ -43,7 +57,7 @@ router.post('/signup',upload.none(), (req, res) => {
 
                 bcrypt.hash(password, 10).then(hashedPassword => {
                     const newUser = new User({
-                        first_name,
+                        firstName,
                         email,
                         password: hashedPassword,
 
@@ -53,7 +67,6 @@ router.post('/signup',upload.none(), (req, res) => {
                         res.json({
                             status: "SUCCESS",
                             message: "Signup Successful",
-                            data: result
                         })
                     }).catch(err => {
                         res.json({
@@ -103,8 +116,7 @@ router.post('/signin',upload.none(), (req, res) => {
                         // Password matched
                         res.json({
                             status: "SUCCESS",
-                            message: "Sign-in successful",
-                            data: data
+                            message: "Sign-in successful"
                         })
                     } else {
                         // Incorrect password
@@ -142,7 +154,6 @@ router.get('/register',upload.none(),function(req,res){
 
             res.send(users);
          }else{
-            console.log("ahaa kollaalo");
            res.send(err);
          }
 
@@ -154,65 +165,35 @@ router.get('/register/:id',upload.none(),function(req,res){
 
             res.send(users);
          }else{
-            console.log("ahaa kollaalo");
+            // console.log("ahaa kollaalo");
            res.send(err);
          }
 
     });
 });
 router.post('/register/',upload.none(),function(req,res){
-  let {first_name,middle_name,last_name,email,age,aadhaar,phone,dob,gender,password} = req.body;
-  first_name  = first_name.trim();
-  middle_name = middle_name.trim();
-  last_name   = last_name.trim();
-  email       = email.trim();
-  aadhaar     = aadhaar.trim();
-  phone       = phone.trim();
-  dob         = dob.trim();
-  gender      = gender.trim();
+  let {firstName,middleName,lastName,email,age,aadhaar,phone,dob,gender,password} = req.body;
+  firstName  = firstName.toString().trim();
+  middleName = middleName.toString().trim();
+  lastName   = lastName.toString().trim();
+  email       = email.toString().trim();
+  aadhaar     = aadhaar.toString().trim();
+  phone       = phone.toString().trim();
+  dob         = dob.toString().trim();
+  gender      = gender.toString().trim();
 
-  if (first_name == "" || last_name == "" || email == "" || age == "" || dob == "" || gender == "" || phone == "") {
+  if (firstName == "" || lastName == "" || email == "" || age == "" || dob == "" || gender == "" || phone == "") {
         res.json({
             status: "FAILED",
             message: "Empty input field(s)"
         })
-  }
-  else if (!/^[\w-\.]+@([\w-]+\.)+[\w-]{2,4}$/.test(email)) {
-              res.json({
-                  status: "FAILED",
-                  message: "Invalid email"
-              })
-  }
-  else if(aadhaar.length != 12) {
-    res.json({
-        status: "FAILED",
-        message: "Invalid aadhaar no."
-    })
-  }
-  else if(gender!="Female" && gender!="Male" && gender!="Other"){
-
-    res.json({
-        status: "FAILED",
-        message: "Invalid gender"
-    })
-  }
-  else if(phone.length!=10){
-    res.json({
-        status: "FAILED",
-        message: "Invalid phone number format"
-    })
-  }
-  else if (!new Date(dob).getTime()) { //mm-dd-yyyy or yyyy-mm-dd
-        res.json({
-            status: "FAILED",
-            message: "Invalid date of birth"
-        })
+        console.log(req.body)
   }
   else{
     const user = new User ({
-      first_name:   req.body.first_name,
-      middle_name: req.body.middle_name,
-      last_name:   req.body.last_name,
+      firstName:   req.body.firstName,
+      middleName: req.body.middleName,
+      lastName:   req.body.lastName,
       email:       req.body.email,
       age :        req.body.age,
       aadhaar:      req.body.aadhaar,
@@ -223,30 +204,228 @@ router.post('/register/',upload.none(),function(req,res){
     });
     user.save(function(err){
       if(err){
-        res.send(err);
+        res.json({error_message: /:(.+)/.exec(err.message)[1], status:"Failed"});
       }else{
-        res.send("Success");
+        res.json({
+            status: "SUCCESS",
+        });
       }
     });
   }
 
 });
 
-
 router.patch('/register/:id',upload.none(), function(req,res){
+    let {firstName,middleName,lastName,email,age,aadhaar,phone,dob,gender} = req.body;
+//   firstName  = firstName.toString().trim();
+//   middleName = middleName.toString().trim();
+//   lastName   = lastName.toString().trim();
+//   email       = email.toString().trim();
+//   aadhaar     = aadhaar.toString().trim();
+//   phone       = phone.toString().trim();
+//   dob         = dob.toString().trim();
+//   gender      = gender.toString().trim();
 
+  if (!(firstName && lastName && email && age && dob && gender && phone)) {
+        res.json({
+            status: "FAILED",
+            message: "Empty input field(s)"
+        })
+        //console.log(req.body)
+  }
+  else
+  {
 
    User.updateOne(
      {_id  : req.params.id},
-     {$set: req.body},
+     {$set: req.body},{runValidators: true} ,
       function(err){
         if(err){
-          res.send(err);
-        }else{
-          res.send("Success");
-        }
-      });
+            res.json({error_message: /:(.+)/.exec(err.message)[1], status:"Failed"});
+          }else{
+            res.json({
+                status: "SUCCESS",
+            });
+          }
+        });
+      }
+    
+    });
 
+
+router.get('/application/:id',upload.fields([{name:'imgPhotograph',maxCount:1},{name:'imgSign',maxCount:1}]),function(req,res){
+    if(req.body.button=="",req.body.button=="save")
+     User.findOne({_id:req.params.id},function(err,users){
+          if(!err) {
+
+            res.send(users);
+         }else{
+            // console.log("ahaa kollaalo");
+           res.send(err);
+         }
+
+    });
+    else if (req.body.button=="submit")
+    {
+        User.findOne({_id:req.params.id},function(err,users){
+            if(!err) {
+                {
+                    let{
+                        nationality,motherTongue,bloodGroup,aPhone,annualIncome,bp1,bp2,bp3,hostelFacility,busFacility,imgPhotograph,imgSign}=users;
+                    a=users;
+                    console.log(users)
+                    if(!(nationality && motherTongue && bloodGroup && aPhone && annualIncome))
+                    {
+                        res.json({
+                            status:"FAILED",
+                            message:"All fields are required"
+                            
+                        })
+                    }
+                    else
+                        if(!(a.contactAddress.addressL1 && a.contactAddress.city && a.contactAddress.state && a.contactAddress.pincode))
+                            {res.json({
+                                status:"FAILED",
+                                message:"Contact Address is incomplete"
+                                
+                            })
+                            console.log(req.body)
+                            console.log(req.body.contactAddress.addressL1)
+                            //res.redirect()
+        
+                        }
+                        else
+                            if(!req.body.c_p && !( a.permanentAddress.city && a.permanentAddress.state && a.permanentAddress.pincode))
+                            res.json({
+                                status:"FAILED",
+                                message:"Permanant Address is incomplete"
+                            
+                                
+                            })
+                            else{
+        
+                                if(!(a.fatherDetails.name && a.fatherDetails.occupation && a.fatherDetails.mobile && a.motherDetails.name && a.motherDetails.occupation && a.motherDetails.mobile && a.guardianDetails.name && a.guardianDetails.relation && a.guardianDetails.mobile ))
+                                    res.json({
+                                        status:"FAILED",
+                                        message:"Parent / Guardian is incomplete"
+                                        
+                                    })
+                                
+                                else 
+                                    if(!(a.NRIdetails.name && a.NRIdetails.relation))
+                                        res.json({
+                                            status:"FAILED",
+                                            message:"NRI sponser details is incomplete"
+                                            
+                                        })
+                                    else
+                                        if(!(bp1 && bp2 && bp3))
+                                            res.json({
+                                                status:"FAILED",
+                                                message:"Atleast 3 branchs should be kept"
+                                                
+                                            })
+                                        else
+                                            {
+                                                ac=a.academicDetails
+                                                if(!(ac.qualifyingExam && ac.phyMarkObtained && ac.phyMaxMarks && ac.chemMarkObtained && ac.chemMaxMarks && ac.mathsMarkObtained && ac.mathsMaxMarks))
+                                                {
+                                                    res.json({
+                                                        status:"FAILED",
+                                                        message:"Academic details incomplete"
+                                                        
+                                                    });
+                                                }
+                                                else
+                                                    if(!(a.imgSign && a.imgPhotograph))
+                                                    {
+                                                        res.json({
+                                                            status:"FAILED",
+                                                            message:"Uploads are Missing"
+                                                            
+                                                        });
+                                                    }
+
+                                                    else
+                                                    res.json({
+                                                        status:"Submitted",
+                                                        
+                                                    })
+                                        }
+                                        
+                                    }
+                }
+               
+    
+                
+           }else{
+             res.json({
+                 status:'FAILED',
+                 message:'Not registered'
+             })
+           }
+    
+      });
+    }
+    
+
+    
 });
+
+router.patch('/application/:id',upload.fields([{name:'imgPhotograph',maxCount:1},{name:'imgSign',maxCount:1}]),function(req,res){
+
+    //edit is clicked
+
+
+    //adding url of photograph to body
+    if(req.files.imgPhotograph)
+    {
+        console.log('img1 uploaded\n')
+        req.body.imgPhotograph=req.files.imgPhotograph[0].path;
+    }
+    //adding url of sign to body
+    if(req.files.imgSign)
+    {
+        console.log('img2 uploaded\n')
+        req.body.imgSign=req.files.imgSign[0].path;
+    }
+    // console.log(req.files)
+    User.findOne({_id:req.params.id},function(err,users){
+        if(!err) {
+           
+            if(req.body.c_p)
+            { 
+                req.body.permanentAddress = req.body.contactAddress ? req.body.contactAddress: users.contactAddress
+                //console.log("new----"+req.body.permanentAddress)
+
+            }
+           {
+                User.updateOne(
+                    {_id  : req.params.id},
+                    {$set: req.body},{runValidators: true} ,
+                    function(err){
+                    if(err){
+                        res.json({error_message: /:(.+)/.exec(err.message)[1], status:"Failed"});
+                        }else{
+                        res.json({
+                            status: "SUCCESS ",
+                        });
+                        }
+                    });
+            }
+
+            
+       }else{
+         res.json({
+             status:'FAILED',
+             message:'Not registered'
+         })
+       }
+
+  });
+    
+    
+    
+})
 
 module.exports = router
