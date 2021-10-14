cacd06157b9f551f19561ad08f6e03ca640e1b8b
Abhiram V <abhi.raa.man.v@gmail.com>
2021-10-14 16:52:18 +0530

    Move login and signup to auth.js

--- api/User.js
+++ api/User.js
@@ -35,140 +35,15 @@ const upload = require('./../handler/multer')
 // const datauri=require('./../handler/datauri')
 
 // mongodb user model
-const User = require('./../models/User')
+const User = require('../models/User')
+const Auth = require('../controllers/auth')
 
 const bcrypt = require('bcrypt')
 
 
-router.post('/signup', upload, (req, res) => {
-    let { firstName, email, password } = req.body
-    console.log(req.body)
-    // first_name = first_name;
-    // email = email;
+router.post('/signup', Auth.signup);
 
-    if (firstName == "" || email == "" || password == "") {
-        res.json({
-            status: "FAILED",
-            message: "Empty input field(s)"
-        })
-    } else if (password.length < 8) {
-        res.json({
-            status: "FAILED",
-            message: "Password is too short"
-        })
-    } else if (!/^[\w-\.]+@([\w-]+\.)+[\w-]{2,4}$/.test(email)) {
-        res.json({
-            status: "FAILED",
-            message: "Invalid email"
-        })
-    } else {
-        // Check if user already exists
-        User.find({ email }).then(result => {
-            if (result.length) {
-                // A user already exists
-                res.json({
-                    status: "FAILED",
-                    message: "User with given email already exists"
-                })
-            } else {
-
-                bcrypt.hash(password, 10).then(hashedPassword => {
-                    const newUser = new User({
-                        firstName,
-                        email,
-                        password: hashedPassword,
-
-                    })
-
-                    newUser.save().then(result => {
-                        res.json({
-                            status: "SUCCESS",
-                            message: "Signup Successful",
-                        })
-                    }).catch(err => {
-                        res.json({
-                            status: "FAILED",
-                            message: "An error occured while adding the user"
-                        })
-                    })
-                }).catch(err => {
-                    res.json({
-                        status: "FAILED",
-                        message: "An error occurred while hashing the password"
-                    })
-                })
-            }
-        }).catch(err => {
-            console.log(err)
-            res.json({
-                status: "FAILED",
-                message: "An error occurred while checking for existance of user"
-            })
-        })
-    }
-});
-
-router.post('/login', upload, (req, res) => {
-    let { email, password } = req.body
-    email = email.trim();
-    console.log(req.body)
-
-    if (email == "" || password == "") {
-        res.json({
-            status: "FAILED",
-            message: "Empty credentials entered"
-        })
-    } else if (!/^[\w-\.]+@([\w-]+\.)+[\w-]{2,4}$/.test(email)) {
-        res.json({
-            status: "FAILED",
-            message: "Invalid email"
-        })
-    } else {
-        User.find({ email }).then(data => {
-            console.log(data)
-            if (data.length) {
-                const hashedPassword = data[0].password
-                bcrypt.compare(password, hashedPassword).then(result => {
-                    if (result) {
-                        const token = jwt.sign(
-                            { user: email },
-                            "secret_key"	// might want to replace
-                        )
-
-                        // Password matched
-                        res.json({
-                            status: "SUCCESS",
-                            message: "Sign-in successful",
-                            token: token,
-                        })
-                    } else {
-                        // Incorrect password
-                        res.json({
-                            status: "FAILED",
-                            message: "Incorrect password or mail"
-                        })
-                    }
-                }).catch(err => {
-                    res.json({
-                        status: "FAILED",
-                        message: "An error occurred while checking the password"
-                    })
-                })
-            } else {    // email not found
-                res.json({
-                    status: "FAILED",
-                    message: "Incorrect password or mail"
-                })
-            }
-        }).catch(err => {
-            console.log(req.body)
-            res.json({
-                status: "FAILED",
-                message: "An error occured while checking for existance of user"
-            })
-        })
-    }
-});
+router.post('/login', Auth.login);
 
 router.patch('/password_change', function (req, res) {
     let { currentPassword, newPassword, token, id } = req.body;
