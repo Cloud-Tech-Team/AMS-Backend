546b808bf6aed370c7e215c694891695bab2d767
Hamda Fizal <hamda.fzl@gmail.com>
2022-01-19 01:18:40 +0530

    Protect application routes by verifying jwt

--- api/User.js
+++ api/User.js
@@ -44,7 +44,7 @@ const Password = require('../controllers/password')
 
 const bcrypt = require('bcrypt')
 
-
+const verifyToken = require('../middleware/verifyToken');
 // router.post('/signup', Auth.signup);
 
 router.post('/login', upload, Auth.login);
@@ -59,7 +59,7 @@ router.patch('/password_change', function (req, res) {
     let { currentPassword, newPassword, token, id } = req.body;
 
 	// Verify the token
-	jwt.verify(token, 'secret_key', function (err, decoded) {
+	jwt.verify(token, process.env.JWT_SECRET_KEY, function (err, decoded) {
 		if (!err) {
 			console.log('token verified');
 			// Find user by id
@@ -275,12 +275,12 @@ router.patch('/register/:id', upload, function (req, res) {
 });
 
 
-router.get('/application/:id', upload, function (req, res) {
+router.get('/application/:id', verifyToken,upload, function (req, res) {
     if (req.body.button == "", req.body.button == "save")
-        User.findOne({ _id: req.params.id }, function (err, users) {
+        User.findOne({ _id: req.params.id }, function (err, user) {
             if (!err) {
 				res.status(200);
-                res.send(users);
+                res.send(user);
             } else {
                 res.send(err);
             }
@@ -388,130 +388,133 @@ router.get('/application/:id', upload, function (req, res) {
 
 });
 
-router.patch('/application/:id', upload, async function (req, res) {
+router.patch('/application/:id',verifyToken, upload, async function (req, res) {
 
-
-    //edit is clicked
-    //adding url of photograph to body
-    if (req.files.imgPhotograph) {
-        console.log('img1 uploaded\n')
-        const file64 = formatBufferTo64(req.files.imgPhotograph[0]);
-        const uploadResult = await cloudinaryUpload(file64.content);
-        req.body.imgPhotograph = uploadResult.secure_url;
-    }
-    //adding url of sign to body
-    if (req.files.imgSign) {
-        console.log('img2 uploaded\n')
-        const file64 = formatBufferTo64(req.files.imgSign[0]);
-        const uploadResult = await cloudinaryUpload(file64.content);
-        req.body.imgSign = uploadResult.secure_url;
-    }
-    // console.log(req.files)
-    User.findOne({ _id: req.params.id }, function (err, users) {
-        if (!err) {
-
-            if (req.body.c_p) {
-                req.body.permanentAddress = req.body.contactAddress ? req.body.contactAddress : users.contactAddress
-            }
-            {
-                a = req.body
-                const update = {
-
-                    firstName: a.firstName || users.firstName || users.a,
-                    middleName: a.middleName || users.middleName || users.a,
-                    lastName: a.lastName || users.lastName || users.a,
-                    email: a.email || users.email || users.a,
-                    age: a.age || users.age || users.a,
-                    aadhaar: a.aadhaar || users.aadhaar || users.a,
-                    phone: a.phone || users.phone || users.a,
-                    aPhone: a.aPhone || users.aPhone || users.a,
-                    dob: a.dob || users.dob || users.a,
-                    gender: a.gender || users.gender || users.a,
-                    password: a.password || users.password || users.a,
-                    nationality: a.nationality || users.nationality || users.a,
-                    motherTongue: a.motherTongue || users.motherTongue || users.a,
-                    bloodGroup: a.bloodGroup || users.bloodGroup || users.a,
-                    contactAddress: {
-                        addressL1: a.addressL1C || users.contactAddress.addressL1 || users.a,
-                        addressL2: a.addressL2C || users.contactAddress.addressL2 || users.a,
-                        city: a.cityC || users.contactAddress.city || users.a,
-                        state: a.stateC || users.contactAddress.state || users.a,
-                        pincode: a.pincodeC || users.contactAddress.pincode || users.a
-                    },
-                    permanentAddress: {
-                        addressL1: a.addressL1P || users.permanentAddress.addressL1 || users.a,
-                        addressL2: a.addressL2P || users.permanentAddress.addressL2 || users.a,
-                        city: a.cityP || users.permanentAddress.city || users.a,
-                        state: a.stateP || users.permanentAddress.state || users.a,
-                        pincode: a.pincodeP || users.permanentAddress.pincode || users.a
-
-                    },
-                    fatherDetails: {
-                        name: a.fatherName || users.fatherDetails.name || users.a,
-                        occupation: a.fatherOccupation || users.fatherDetails.occupation || users.a,
-                        mobile: a.fatherMobile || users.fatherDetails.mobile || users.a,
-                        email: a.fatherEmail || users.fatherDetails.email || users.a
-                    },
-                    motherDetails: {
-                        name: a.motherName || users.motherDetails.name || users.a,
-                        occupation: a.motherOccupation || users.motherDetails.occupation || users.a,
-                        mobile: a.motherMobile || users.motherDetails.moblie || users.a,
-                        email: a.motherEmail || users.motherDetails.email || users.a
-                    },
-                    guardianDetails: {
-                        name: a.guardianName || users.guardianDetails.name || users.a,
-                        relation: a.guardianRelation || users.guardianDetails.relation,
-                        mobile: a.guardianMobile || users.guardianDetails.moblie || users.a,
-                        email: a.guardianEmail || users.guardianDetails.email || users.a
-                    },
-                    annualIncome: a.annualIncome || users.annualIncome || users.a,
-                    NRIdetails: {
-                        name: a.NRIname || users.NRIdetails.name || users.a,
-                        relation: a.NRIrelation || users.NRIdetails.relation || users.a
-                    },
-                    bp1: a.bp1 || users.bp1 || users.a,
-                    bp2: a.bp2 || users.bp2 || users.a,
-                    bp3: a.bp3 || users.bp3 || users.a,
-                    bp4: a.bp4 || users.bp4 || users.a,
-                    bp5: a.bp5 || users.bp5 || users.a,
-                    busFacility: a.busFacility || users.busFacility || users.a,
-                    hostelFacility: a.hostelFacility || users.hostelFacility || users.a,
-                    academicDetails: {
-                        qualifyingExam: a.qualifyingExam || users.qualifyingExam || users.a,
-                        phyMarkObtained: a.phyMarkObtained || users.phyMarkObtained || users.a,
-                        phyMaxMarks: a.phyMaxMarks || users.phyMaxMarks || users.a,
-                        chemMarkObtained: a.chemMarkObtained || users.chemMarkObtained || users.a,
-                        chemMaxMarks: a.chemMaxMarks || users.chemMaxMarks || users.a,
-                        mathsMarkObtained: a.mathsMarkObtained || users.mathsMarkObtained || users.a,
-                        mathsMaxMarks: a.mathsMaxMarks || users.mathsMaxMarks || users.a
-                    },
-                    imgPhotograph: a.imgPhotograph || users.imgPhotograph || users.a,
-                    imgSign: a.imgSign || users.imgSign || users.a
-                }
-                User.updateOne(
-                    { _id: req.params.id },
-                    { $set: update }, { runValidators: true },
-                    function (err) {
-                        if (err) {
-                            res.json({ error_message: /:(.+)/.exec(err.message)[1], status: "FAILED" });
-                        } else {
-                            res.json({
-                                status: "SUCCESS ",
-                            });
-                        }
-                    });
-            }
-        } else {
-            res.json({
-                status: 'FAILED',
-                message: 'Not registered'
-            })
+          //edit is clicked
+        //adding url of photograph to body
+        if (req.files.imgPhotograph) {
+            console.log('img1 uploaded\n')
+            const file64 = formatBufferTo64(req.files.imgPhotograph[0]);
+            const uploadResult = await cloudinaryUpload(file64.content);
+            req.body.imgPhotograph = uploadResult.secure_url;
+        }
+        //adding url of sign to body
+        if (req.files.imgSign) {
+            console.log('img2 uploaded\n')
+            const file64 = formatBufferTo64(req.files.imgSign[0]);
+            const uploadResult = await cloudinaryUpload(file64.content);
+            req.body.imgSign = uploadResult.secure_url;
         }
 
-    });
+        // console.log(req.files)
+        User.findOne({ _id: req.params.id }, function (err, users) {
+            if (!err) {
+
+                if (req.body.c_p) {
+                    req.body.permanentAddress = req.body.contactAddress ? req.body.contactAddress : users.contactAddress
+                }
+                {
+                    a = req.body
+                    const update = {
+
+                        firstName: a.firstName || users.firstName || users.a,
+                        middleName: a.middleName || users.middleName || users.a,
+                        lastName: a.lastName || users.lastName || users.a,
+                        email: a.email || users.email || users.a,
+                        age: a.age || users.age || users.a,
+                        aadhaar: a.aadhaar || users.aadhaar || users.a,
+                        phone: a.phone || users.phone || users.a,
+                        aPhone: a.aPhone || users.aPhone || users.a,
+                        dob: a.dob || users.dob || users.a,
+                        gender: a.gender || users.gender || users.a,
+                        password: a.password || users.password || users.a,
+                        nationality: a.nationality || users.nationality || users.a,
+                        motherTongue: a.motherTongue || users.motherTongue || users.a,
+                        bloodGroup: a.bloodGroup || users.bloodGroup || users.a,
+                        contactAddress: {
+                            addressL1: a.addressL1C || users.contactAddress.addressL1 || users.a,
+                            addressL2: a.addressL2C || users.contactAddress.addressL2 || users.a,
+                            city: a.cityC || users.contactAddress.city || users.a,
+                            state: a.stateC || users.contactAddress.state || users.a,
+                            pincode: a.pincodeC || users.contactAddress.pincode || users.a
+                        },
+                        permanentAddress: {
+                            addressL1: a.addressL1P || users.permanentAddress.addressL1 || users.a,
+                            addressL2: a.addressL2P || users.permanentAddress.addressL2 || users.a,
+                            city: a.cityP || users.permanentAddress.city || users.a,
+                            state: a.stateP || users.permanentAddress.state || users.a,
+                            pincode: a.pincodeP || users.permanentAddress.pincode || users.a
+
+                        },
+                        fatherDetails: {
+                            name: a.fatherName || users.fatherDetails.name || users.a,
+                            occupation: a.fatherOccupation || users.fatherDetails.occupation || users.a,
+                            mobile: a.fatherMobile || users.fatherDetails.mobile || users.a,
+                            email: a.fatherEmail || users.fatherDetails.email || users.a
+                        },
+                        motherDetails: {
+                            name: a.motherName || users.motherDetails.name || users.a,
+                            occupation: a.motherOccupation || users.motherDetails.occupation || users.a,
+                            mobile: a.motherMobile || users.motherDetails.moblie || users.a,
+                            email: a.motherEmail || users.motherDetails.email || users.a
+                        },
+                        guardianDetails: {
+                            name: a.guardianName || users.guardianDetails.name || users.a,
+                            relation: a.guardianRelation || users.guardianDetails.relation,
+                            mobile: a.guardianMobile || users.guardianDetails.moblie || users.a,
+                            email: a.guardianEmail || users.guardianDetails.email || users.a
+                        },
+                        annualIncome: a.annualIncome || users.annualIncome || users.a,
+                        NRIdetails: {
+                            name: a.NRIname || users.NRIdetails.name || users.a,
+                            relation: a.NRIrelation || users.NRIdetails.relation || users.a
+                        },
+                        bp1: a.bp1 || users.bp1 || users.a,
+                        bp2: a.bp2 || users.bp2 || users.a,
+                        bp3: a.bp3 || users.bp3 || users.a,
+                        bp4: a.bp4 || users.bp4 || users.a,
+                        bp5: a.bp5 || users.bp5 || users.a,
+                        busFacility: a.busFacility || users.busFacility || users.a,
+                        hostelFacility: a.hostelFacility || users.hostelFacility || users.a,
+                        academicDetails: {
+                            qualifyingExam: a.qualifyingExam || users.qualifyingExam || users.a,
+                            phyMarkObtained: a.phyMarkObtained || users.phyMarkObtained || users.a,
+                            phyMaxMarks: a.phyMaxMarks || users.phyMaxMarks || users.a,
+                            chemMarkObtained: a.chemMarkObtained || users.chemMarkObtained || users.a,
+                            chemMaxMarks: a.chemMaxMarks || users.chemMaxMarks || users.a,
+                            mathsMarkObtained: a.mathsMarkObtained || users.mathsMarkObtained || users.a,
+                            mathsMaxMarks: a.mathsMaxMarks || users.mathsMaxMarks || users.a
+                        },
+                        imgPhotograph: a.imgPhotograph || users.imgPhotograph || users.a,
+                        imgSign: a.imgSign || users.imgSign || users.a
+                    }
+                    User.updateOne(
+                        { _id: req.params.id },
+                        { $set: update }, { runValidators: true },
+                        function (err) {
+                            if (err) {
+                                res.json({ error_message: /:(.+)/.exec(err.message)[1], status: "FAILED" });
+                            } else {
+                                res.json({
+                                    status: "SUCCESS ",
+                                });
+                            }
+                        });
+                }
+            } else {
+                res.json({
+                    status: 'FAILED',
+                    message: 'Not registered'
+                })
+            }
+
+        });
 
 
 
-})
+
+
+});
+
 
 module.exports = router
