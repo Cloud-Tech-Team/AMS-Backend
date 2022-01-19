c1e7e2165fbaca4722bd4519b6d2654a3173648d
Hamda Fizal <hamda.fzl@gmail.com>
2022-01-19 13:46:13 +0530

    change name img to file

--- handler/multer.js
+++ handler/multer.js
@@ -3,8 +3,8 @@ const multer =require('multer'); // form-data multipart
 
 
 const storage = multer.memoryStorage();
-const multerUploads = multer({ storage }).fields([{name:'imgPhotograph',maxCount:1},{name:'imgSign',maxCount:1}]);
-// const multerUploads = multer({ storage }).single('imgPhotograph');
+const multerUploads = multer({ storage }).fields([{name:'filePhotograph',maxCount:1},{name:'fileSign',maxCount:1},{name:'fileTransactionID',maxCount:1}]);
+
 
 const path=require('path')
 
