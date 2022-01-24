b59b005455f763ea1530c4604d5b3bba00612821
Hanna Sabu Mampallil <hanblessab@gmail.com>
2022-01-24 20:09:43 +0530

    cors

--- middleware/verifyToken.js
+++ middleware/verifyToken.js
@@ -4,7 +4,7 @@ module.exports = (req, res, next) => {
         
         try {
           
-          comsole.log(req.headers.authorization)
+          console.log(req.headers.authorization)
           const token = req.headers.authorization.split(" ")[1];
           const decoded = jwt.verify(token,process.env.JWT_SECRET_KEY);
           console.log(decoded);
