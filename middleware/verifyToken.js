17a88710a63d474d2f310429aa4f8b0af8700eff
Hanna Sabu Mampallil <hanblessab@gmail.com>
2022-01-24 17:52:23 +0530

    cors changes

--- middleware/verifyToken.js
+++ middleware/verifyToken.js
@@ -1,16 +1,18 @@
 const jwt = require('jsonwebtoken');
 
 module.exports = (req, res, next) => {
+        
         try {
           const token = req.headers.authorization.split(" ")[1];
           const decoded = jwt.verify(token,process.env.JWT_SECRET_KEY);
           req.userData = decoded;
           next();
         } catch(err) {
-
+            console.log(err.message)
             return res.status(401).json({
-                message: "Auth failed"
+                message: err.message
             })
+
         }
 
 };
