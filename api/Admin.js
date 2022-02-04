2e5ac3f8a884c9b4f2ce1aa92ecbafe77da3a058
Abhiram V <abhi.raa.man.v@gmail.com>
2022-02-04 17:44:41 +0000

    Add new api /count to route /admin
    The request body of /count takes a JWT token and an arbitrary number of
    fields. After verifying the token, it is removed from the body. The
    remaining body is then used to query the user database. The count of
    users that match the query is returned.

--- api/Admin.js
+++ api/Admin.js
@@ -4,4 +4,40 @@ const upload = require('../handler/multer')
 const jwt = require('jsonwebtoken')
 const User = require('../models/User')
 
+router.get('/count', upload, function (req, res) {
+	console.log('/count\n=======\n');
+	console.log(req.body);
+
+	if (req.body.token) {
+		token = req.body.token;
+		const decoded = jwt.verify(token, process.env.JWT_SECRET_KEY);
+		console.log('email: ' + decoded.email + ' role: ' + decoded.role);
+		if (decoded.role != 'admin') {
+			console.log('User is not admin!');
+			res.status(403);	// Forbidden
+			res.json({
+				status: "FAILED",
+				message: "Access denied"
+			});
+		} else {
+			delete req.body.token;
+			User.find(req.body, function (err, result) {
+				if (err) {
+					res.status(500);	// Internal server error
+					console.log('error message:\n' + err.message);
+					res.json({
+						status: "FAILED",
+						message: "Internal server error"
+					});
+				} else {
+					res.status(200);
+					res.json({
+						status: "SUCCESS",
+						count: result.length
+					});
+				}
+			});
+		}
+	}
+})
 module.exports = router
