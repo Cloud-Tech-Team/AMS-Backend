2375ff9b76125c58727dcc13070451d14a3bdd92
Abhiram V <abhi.raa.man.v@gmail.com>
2022-11-13 22:27:39 +0530

    Remove options from cors()

--- server.js
+++ server.js
@@ -8,14 +8,14 @@ const path = require('path');
 //cors policy
 const cors=require('cors');
 
-var options = {
-    "origin": "*",
-    "methods": "GET,HEAD,PUT,PATCH,POST,DELETE",
-    // "preflightContinue": false,
-    // "optionsSuccessStatus": 204
-  }
+//var options = {
+//    "origin": "*",
+//    "methods": "GET,HEAD,PUT,PATCH,POST,DELETE",
+//    // "preflightContinue": false,
+//    // "optionsSuccessStatus": 204
+//  }
 
-app.use(cors(options));
+app.use(cors());
 // app.options('*',cors());
 
 const bodyParser = require('body-parser')
