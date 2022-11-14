2fa8453781155f62ca843f6049331aee0ad9ccab
hamda-fizal <hamda.fzl@gmail.com>
2022-11-14 13:52:29 +0530

    patch application preview file

--- handler/multer.js
+++ handler/multer.js
@@ -3,7 +3,8 @@ const multer =require('multer'); // form-data multipart
 
 
 const storage = multer.memoryStorage();
-const multerUploads = multer({ storage }).fields([{name:'filePhotograph',maxCount:1},{name:'imgSign',maxCount:1},{name:'fileTransactionID',maxCount:1},{name:'file12th',maxCount:1},{name:'file10th',maxCount:1},{name:'fileKeam',maxCount:1}]);
+const multerUploads = multer({ storage }).fields([{name:'filePhotograph',maxCount:1},{name:'imgSign',maxCount:1},{name:'fileTransactionID',maxCount:1},
+{name:'file12th',maxCount:1},{name:'file10th',maxCount:1},{name:'fileKeam',maxCount:1},{name:'filePreview',maxCount:1}]);
 
 
 const path=require('path')
--- models/User.js
+++ models/User.js
@@ -290,7 +290,9 @@ const UserSchema = new Schema({
       fileTransactionID:{
         type:String
       },
-
+      filePreview: {
+        type:String,
+      },
       registrationTimeStamp : {
         type:Date,
         default: Date.now()
